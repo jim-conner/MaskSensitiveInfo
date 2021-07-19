2756dcb1e5e650f1419ef8cd1d8d7eebd970b466
jim-conner <jamesdavidconner@gmail.com>
2021-07-19 18:13:12 -0500

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a secret...");
+            string secret = Console.ReadLine();
+            
+
+
+            if (secret.Length > 4)
+            {
+                string result = secret.Substring(secret.Length - 4)
+                .PadLeft(secret.Length, '*');
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Try a longer word next time.");
+            }
+        }
+        //this helped me
+        //https://stackoverflow.com/questions/9035192/mask-out-part-first-12-characters-of-string-with/9037991
+    }
+}
